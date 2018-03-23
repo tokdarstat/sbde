ca1cfa59d2785c0dc8ace1489247798aaaef168b
Surya Tokdar <tokdar@stat.duke.edu>
2018-03-22 22:46:24 -0400

    Create demo.R

---
+++ demo.R
@@ -0,0 +1,17 @@
+require(sbde)
+require(evd)
+
+n <- 1000
+ix <- (runif(n) < 0.5)
+y <- rep(NA, n)
+y[ix] <- rnorm(sum(ix), -4, 1)
+y[!ix] <- rgpd(sum(!ix), shape = 0.5)
+
+o <- sbde(y, nknots = 11, nsamp = 1000, thin = 30) 
+summary(o, more = TRUE)
+print(coef(o))
+pp <- predict(o)
+
+par(mfrow = c(1,1))
+hist(y, 200, freq = FALSE, col = "gray", border = "white")
+for(i in 1:3) lines(pp$y, pp$fest[,i], lty = 1 + (i != 2))
